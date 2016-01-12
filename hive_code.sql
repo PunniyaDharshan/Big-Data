a6655d79a90d7e89d247878ec6620a3ce1395da2
PunniyaDharshan <pganesa1@uncc.edu>
2016-01-12 12:18:13 -0500

    Project Source code

--- hive_code.sql
+++ hive_code.sql
@@ -51,6 +51,34 @@ as Title
 from patent_ipgb where file like '%invention-title%'
 ---------------------------------------------------------------------------------------------------------------------------------------
 
+/*Code used to derive the  Factual Fields:*/
+
+
+/*Code used to Extract Days Information:*/
+select ((cast(substr(date_grant,1,4) as int)-cast(substr(date_publ,1,4) as int))*365) 
++ 
+case when cast(substr(date_grant,5,2) as int)>=cast(substr(date_publ,5,2) as int) 
+then (cast(substr(date_grant,5,2) as int)-cast(substr(date_publ,5,2) as int))*30
+else (cast(substr(date_publ,5,2) as int)-cast(substr(date_grant,5,2) as int))*30
+end
+   + 
+case when cast(substr(date_grant,7,2) as int)>=cast(substr(date_publ,7,2) as int) 
+then (cast(substr(date_grant,7,2) as int)-cast(substr(date_publ,7,2) as int))
+else (cast(substr(date_publ,7,2) as int)-cast(substr(date_grant,7,2) as int))
+end as Granted_days
+from days
+
+-------------------------------------------------------------------------------------------------------------------------------
+/*Code used to Extract Backward and Forward Citations:*/
+
+select substr(file,(instr(file,'le="'))+4,(instr(file,'.XML'))-(instr(file,'file='))-6) as patentno_datepubl,
+(substr(file,(instr(file,'<doc-number>'))+12,(instr(file,'</doc-number>'))-13)) as Backward_citation,
+case when (substr(file,(instr(file,'<category>'))+10,(instr(file,'</category>'))-11)) like 'cited by%' 
+then 1 else 0 end as Forward_citation
+from patent_ipgb where file like '%us-patent-grant%' or file like '<category>%'
+-------------------------------------------------------------------------------------------------------------------------------
+
+
 
 
 
